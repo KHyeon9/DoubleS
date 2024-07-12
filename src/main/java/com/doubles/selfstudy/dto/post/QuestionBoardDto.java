fcf6cb2f8901b448b56086ebfcbe05598d3a7e7d
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-12 19:05:50 +0900

    QuestionBoardDto에서 auditing부분 추가 및 of와 from메소드 추가

--- src/main/java/com/doubles/selfstudy/dto/post/QuestionBoardDto.java
+++ src/main/java/com/doubles/selfstudy/dto/post/QuestionBoardDto.java
@@ -1,22 +1,44 @@
 package com.doubles.selfstudy.dto.post;
 
-import com.doubles.selfstudy.entity.UserAccount;
-import jakarta.persistence.*;
+import com.doubles.selfstudy.dto.user.UserAccountDto;
+import com.doubles.selfstudy.entity.QuestionBoard;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 public class QuestionBoardDto {
 
     private Long id;
-    private UserAccount userAccount; // 유저 정보
+    private UserAccountDto userAccountDto; // 유저 정보
     private String title; // 제목
     private String content; // 내용
     private QuestionBoardTag tag; // 태그
+    private LocalDateTime createdAt; // 생성 일시
+    private String createdBy; // 생성자
+    private LocalDateTime modifiedAt; // 수정 일시
+    private String modifiedBy; // 수정자
 
+    public static QuestionBoardDto of(UserAccountDto userAccountDto, String title, String content, QuestionBoardTag tag) {
+        return new QuestionBoardDto(null, userAccountDto, title, content, tag, null, null, null, null);
+    }
 
+    public static QuestionBoardDto of(Long id, UserAccountDto userAccountDto, String title, String content, QuestionBoardTag tag, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new QuestionBoardDto(id, userAccountDto, title, content, tag, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static QuestionBoardDto fromEntity(QuestionBoard entity) {
+        return new QuestionBoardDto(
+                entity.getId(),
+                UserAccountDto.fromEntity(entity.getUserAccount()),
+                entity.getTitle(),
+                entity.getContent(),
+                entity.getTag(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
 }
