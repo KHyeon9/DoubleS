5315c1c9681f91f96e96cee1727038b302409577
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-12 18:55:58 +0900

    UserAccount에 주석 추가 및 DTO 추가

---
+++ src/main/java/com/doubles/selfstudy/dto/user/UserAccountDto.java
@@ -0,0 +1,67 @@
+package com.doubles.selfstudy.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserAccountDto implements UserDetails {
+
+    private String userId; // 유저 ID
+    private String password; // 비밀번호
+    private RoleType roleType; // roleType
+    private String email; // 이메일
+    private String nickname; // 닉네임
+    private String memo; // 메로(자기 소개 비슷하게 사용)
+    private LocalDateTime createdAt; // 생성 일시
+    private String createdBy; // 생성자
+    private LocalDateTime modifiedAt; // 수정 일시
+    private String modifiedBy; // 수정자
+
+    public static UserAccountDto of(String userId, String password, RoleType roleType, String email, String nickname, String memo) {
+        return new UserAccountDto(userId, password, roleType, email, nickname, memo, null, null, null, null);
+    }
+
+    public static UserAccountDto of(String userId, String password, RoleType roleType, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new UserAccountDto(userId, password, roleType, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static UserAccountDto fromEntity(UserAccountDto entity) {
+        return new UserAccountDto(
+                entity.getUserId(),
+                entity.getPassword(),
+                entity.getRoleType(),
+                entity.getEmail(),
+                entity.getNickname(),
+                entity.getMemo(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(roleType.getRoleName()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return nickname;
+    }
+}
