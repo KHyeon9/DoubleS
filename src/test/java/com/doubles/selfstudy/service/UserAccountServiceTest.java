5b71995c5419f91209d3cff964be946b3cff489e
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-13 18:52:10 +0900

    회원가입 실패시 테스트 케이스 작성

--- src/test/java/com/doubles/selfstudy/service/UserAccountServiceTest.java
+++ src/test/java/com/doubles/selfstudy/service/UserAccountServiceTest.java
@@ -1,6 +1,7 @@
 package com.doubles.selfstudy.service;
 
 import com.doubles.selfstudy.entity.UserAccount;
+import com.doubles.selfstudy.exception.DoubleSApplicationException;
 import com.doubles.selfstudy.repository.UserAccountRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.Optional;
 
 import static com.doubles.selfstudy.fixture.UserAccountFixture.get;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -38,5 +40,24 @@ class UserAccountServiceTest {
 
         // Then
         assertDoesNotThrow(() -> userAccountService.regist(userId, password, email, nickname));
+    }
+
+    @Test
+    void 회원가입시_아이디가_중복되면_에러를_반환() {
+        // Given
+        String userId = "userId";
+        String password = "password";
+        String email = "test@email.com";
+        String nickname = "nickname";
+
+        UserAccount fixture = get(userId, password, email, nickname);
+
+        // When
+        when(userAccountRepository.findById(userId)).thenReturn(Optional.of(fixture));
+        when(userAccountRepository.save(any())).thenReturn(Optional.of(fixture));
+
+        // Then
+        assertThrows(DoubleSApplicationException.class,
+                () -> userAccountService.regist(userId, password, email, nickname));
     }
 }
