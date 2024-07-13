25ae999d33043f98bd8fd9635801e902cdc78b62
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-13 20:36:52 +0900

    추가 에러 코드 작성

--- src/main/java/com/doubles/selfstudy/exception/ErrorCode.java
+++ src/main/java/com/doubles/selfstudy/exception/ErrorCode.java
@@ -7,7 +7,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    DUPLICATED_USER_ID(HttpStatus.CONFLICT, "User ID is duplicated"),;
+    DUPLICATED_USER_ID(HttpStatus.CONFLICT, "User ID is duplicated"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
+    ;
 
     private HttpStatus status;
     private String message;
