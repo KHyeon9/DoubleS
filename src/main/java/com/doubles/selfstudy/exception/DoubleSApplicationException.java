86fbf205e071ad6cc6a556714162dc5014143079
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-13 20:37:31 +0900

    DoubleSApplicationException의 메소드 추가
    메세지가 필요없는 경우 사용할 메소드 추가

--- src/main/java/com/doubles/selfstudy/exception/DoubleSApplicationException.java
+++ src/main/java/com/doubles/selfstudy/exception/DoubleSApplicationException.java
@@ -10,6 +10,12 @@ public class DoubleSApplicationException extends RuntimeException{
     private ErrorCode errorCode;
     private String message;
 
+    // 메세지가 필요없는 경우 메소드
+    public DoubleSApplicationException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.message = null;
+    }
+
     @Override
     public String getMessage() {
         if (message == null) {
