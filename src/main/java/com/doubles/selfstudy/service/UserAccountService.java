31b86f3b079f20922492e3133f60e3f03d118f0f
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-16 01:11:15 +0900

    UserAccountService의 regist 수정 및 value추가
    jwt를 사용할 것이기 때문에 key와 expiredTime을 가져오는 변수 추가 및 @Transactional, 보안을 위한 password 인코더 추가

--- src/main/java/com/doubles/selfstudy/service/UserAccountService.java
+++ src/main/java/com/doubles/selfstudy/service/UserAccountService.java
@@ -5,24 +5,55 @@ import com.doubles.selfstudy.entity.UserAccount;
 import com.doubles.selfstudy.exception.DoubleSApplicationException;
 import com.doubles.selfstudy.exception.ErrorCode;
 import com.doubles.selfstudy.repository.UserAccountRepository;
+import com.doubles.selfstudy.utils.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserAccountService {
 
     private final UserAccountRepository userAccountRepository;
+    private final BCryptPasswordEncoder encoder;
 
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${jwt.token.expired-time-ms}")
+    private Long expiredTimeMs;
+
+    @Transactional
     public UserAccountDto regist(String userId, String password, String email, String nickname) {
-        // 회원가입하는 userId 중복 체크
+        // 회원 가입하는 userId 중복 체크
         userAccountRepository.findById(userId).ifPresent(it -> {
-            throw new DoubleSApplicationException(ErrorCode.DUPLICATED_USER_ID, String.format("%s is duplicated", userId));
+            throw new DoubleSApplicationException(ErrorCode.DUPLICATED_USER_ID, String.format("'%s' 는 있는 아이디입니다.", userId));
         });
 
-        // 회원가입 진행
-        UserAccount userAccount = userAccountRepository.save(UserAccount.of(userId, password, email, nickname, null));
+        // 회원 가입 진행
+        UserAccount userAccount = userAccountRepository.save(
+                UserAccount.of(userId, encoder.encode(password), email, nickname, null));
 
         return UserAccountDto.fromEntity(userAccount);
+    }
+
+    public String login(String userId, String password) {
+        // 회원 가입 체크
+        UserAccount userAccount = userAccountRepository.findById(userId).orElseThrow(() ->
+                new DoubleSApplicationException(
+                        ErrorCode.USER_NOT_FOUND, String.format("'%s' 를 찾지 못했습니다.", userId))
+        );
+
+        // 비밀 번호 체크
+        if (!encoder.matches(password, userAccount.getPassword())) {
+            throw new DoubleSApplicationException(ErrorCode.INVALID_PASSWORD);
+        }
+
+        // 토큰 생성
+        String token = JwtTokenUtils.createJwtToken(userId, secretKey, expiredTimeMs);
+
+        return token;
     }
 }
