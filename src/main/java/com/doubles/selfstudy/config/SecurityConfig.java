00752a778f814cfb137b2d7c8d72314c9b8a1ad3
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-16 01:07:05 +0900

    SecurityConfig에 csrf를 disabled 설정

--- src/main/java/com/doubles/selfstudy/config/SecurityConfig.java
+++ src/main/java/com/doubles/selfstudy/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.doubles.selfstudy.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,7 +13,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(auth -> auth
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/**").permitAll()
                         .anyRequest().permitAll()
                 )
