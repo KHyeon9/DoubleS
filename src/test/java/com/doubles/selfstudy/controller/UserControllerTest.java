cb9748df79a1c60ad95fb04a7be8fdbd79bf191b
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-16 19:40:34 +0900

    UserControllerTest에 로그인 테스트 케이스 작성

--- src/test/java/com/doubles/selfstudy/controller/UserControllerTest.java
+++ src/test/java/com/doubles/selfstudy/controller/UserControllerTest.java
@@ -1,10 +1,13 @@
 package com.doubles.selfstudy.controller;
 
+import com.doubles.selfstudy.controller.request.UserLoginRequest;
 import com.doubles.selfstudy.controller.request.UserRegistRequest;
+import com.doubles.selfstudy.controller.response.UserLoginResponse;
 import com.doubles.selfstudy.dto.user.UserAccountDto;
 import com.doubles.selfstudy.exception.DoubleSApplicationException;
 import com.doubles.selfstudy.exception.ErrorCode;
 import com.doubles.selfstudy.service.UserAccountService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +17,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -35,8 +40,7 @@ class UserControllerTest {
     private UserAccountService userAccountService;
 
     @Test
-    @WithAnonymousUser
-    void 회원가입() throws Exception {
+    void 회원가입_정상_작동() throws Exception {
         // Given
         String userId = "userId";
         String password = "password";
@@ -57,7 +61,6 @@ class UserControllerTest {
     }
 
     @Test
-    @WithAnonymousUser
     void 회원가입시_이미_있는_userId로_가입을_하는_경우() throws Exception {
         // Given
         String userId = "userId";
@@ -78,4 +81,59 @@ class UserControllerTest {
                 .andExpect(status().is(ErrorCode.DUPLICATED_USER_ID.getStatus().value()));
     }
 
+    @Test
+    void 로그인_정상_작동() throws Exception {
+        // Given
+        String userId = "userId";
+        String password = "password";
+
+        // When
+        when(userAccountService.login(userId, password)).thenReturn("test_token");
+
+        // Then
+        mockMvc.perform(post("/api/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsBytes(new UserLoginRequest(userId, password)))
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void 로그인시_userId로_가입한_유저가_없는_경우_에러_반환() throws Exception {
+        // Given
+        String userId = "userId";
+        String password = "password";
+
+        // When
+        when(userAccountService.login(userId, password))
+                .thenThrow(new DoubleSApplicationException(ErrorCode.DUPLICATED_USER_ID, ""));
+
+        // Then
+        mockMvc.perform(post("/api/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsBytes(new UserLoginRequest(userId, password)))
+                )
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.DUPLICATED_USER_ID.getStatus().value()));
+    }
+
+    @Test
+    void 로그인시_password가_틀린_경우_에러_반환() throws Exception {
+        // Given
+        String userId = "userId";
+        String password = "password";
+
+        // When
+        when(userAccountService.login(userId, password))
+                .thenThrow(new DoubleSApplicationException(ErrorCode.INVALID_PASSWORD, ""));
+
+        // Then
+        mockMvc.perform(post("/api/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsBytes(new UserLoginRequest(userId,password)))
+                )
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.INVALID_PASSWORD.getStatus().value()));
+    }
 }
