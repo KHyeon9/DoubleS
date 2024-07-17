eb5fb8a8d1f2f4eae865e394c553f3daca484604
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-17 20:36:03 +0900

    게시판 주소 맵핑 수정에 의한 변경사항 적용

--- src/test/java/com/doubles/selfstudy/controller/QuestionBoardControllerTest.java
+++ src/test/java/com/doubles/selfstudy/controller/QuestionBoardControllerTest.java
@@ -2,17 +2,19 @@ package com.doubles.selfstudy.controller;
 
 import com.doubles.selfstudy.controller.request.QuestionBoardCreaeteRequest;
 import com.doubles.selfstudy.exception.ErrorCode;
+import com.doubles.selfstudy.service.QuestionBoardService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,6 +27,9 @@ class QuestionBoardControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockBean
+    private QuestionBoardService questionBoardService;
+
     @Test
     @WithMockUser
     void 질문_게시글_작성_성공() throws Exception {
@@ -33,7 +38,7 @@ class QuestionBoardControllerTest {
         String content = "content";
 
         // When&Then
-        mockMvc.perform(post("/api/post/create")
+        mockMvc.perform(post("/api/main/post/create")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsBytes(new QuestionBoardCreaeteRequest(title, content)))
                 )
@@ -42,18 +47,18 @@ class QuestionBoardControllerTest {
     }
 
     @Test
-    @WithMockUser
+    @WithAnonymousUser
     void 질문_게시글_작성시_로그인이_않된_경우() throws Exception {
         // Given
         String title = "title";
         String content = "content";
 
         // When&Then
-        mockMvc.perform(post("/api/post/create")
+        mockMvc.perform(post("/api/main/post/create")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsBytes(new QuestionBoardCreaeteRequest(title, content)))
                 )
                 .andDo(print())
-                .andExpect(status().is(ErrorCode.USER_NOT_FOUND.getStatus().value()));
+                .andExpect(status().is(ErrorCode.INVALID_TOKEN.getStatus().value()));
     }
 }
