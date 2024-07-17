7cc68a071e27b6c225743fd25610f66c503565ff
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-17 20:46:51 +0900

    JWT 토큰 필터를 SecurityConfig에 적용

--- src/main/java/com/doubles/selfstudy/config/SecurityConfig.java
+++ src/main/java/com/doubles/selfstudy/config/SecurityConfig.java
@@ -1,27 +1,49 @@
 package com.doubles.selfstudy.config;
 
+import com.doubles.selfstudy.config.filter.JwtTokenFilter;
+import com.doubles.selfstudy.exception.CustomAuthenticationEntryPoint;
+import com.doubles.selfstudy.service.UserAccountService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final UserAccountService userAccountService;
+
+    @Value("${jwt.secret-key}")
+    private String key;
+
+    public SecurityConfig(@Lazy UserAccountService userAccountService) {
+        this.userAccountService = userAccountService;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/api/login", "/api/regist").permitAll()
+                        .requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll()
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
+                .addFilterBefore(new JwtTokenFilter(key, userAccountService), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(handle ->
+                            handle.authenticationEntryPoint(
+                                    new CustomAuthenticationEntryPoint()
+                            )
+                        )
                 .build();
     }
 
