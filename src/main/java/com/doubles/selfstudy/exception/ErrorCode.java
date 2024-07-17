b6939966ad9fc3d912cb0461874031682411f4fa
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-17 20:01:29 +0900

    토큰 오류 코드 추가

--- src/main/java/com/doubles/selfstudy/exception/ErrorCode.java
+++ src/main/java/com/doubles/selfstudy/exception/ErrorCode.java
@@ -10,6 +10,7 @@ public enum ErrorCode {
     DUPLICATED_USER_ID(HttpStatus.CONFLICT, "User ID is duplicated"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "token is invalid"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
     ;
 
