e412572c53c21d0e6011a562cac531c69f1891a4
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-17 20:45:18 +0900

    커스텀 인증 엔드포인트 작성
    토큰 인증 실패시 커스텀한 에러를 반환하기 위해서 작성

---
+++ src/main/java/com/doubles/selfstudy/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,23 @@
+package com.doubles.selfstudy.exception;
+
+import com.doubles.selfstudy.controller.response.Response;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    
+    // 토큰 인증 실패시 커스텀한 에러를 반환하기 위함
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.setStatus(ErrorCode.INVALID_TOKEN.getStatus().value());
+        response.getWriter().write(Response.error(ErrorCode.INVALID_TOKEN.name()).toStream());
+    }
+}
