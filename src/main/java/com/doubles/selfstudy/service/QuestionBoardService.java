d53a7150676042d90065bc2e7ad29cd4d25f7e14
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-19 20:57:50 +0900

    Backend: 질문 게시판 수정 서비스 로직 작성

--- src/main/java/com/doubles/selfstudy/service/QuestionBoardService.java
+++ src/main/java/com/doubles/selfstudy/service/QuestionBoardService.java
@@ -1,5 +1,6 @@
 package com.doubles.selfstudy.service;
 
+import com.doubles.selfstudy.dto.post.QuestionBoardDto;
 import com.doubles.selfstudy.entity.QuestionBoard;
 import com.doubles.selfstudy.entity.UserAccount;
 import com.doubles.selfstudy.exception.DoubleSApplicationException;
@@ -31,20 +32,39 @@ public class QuestionBoardService {
 
     // 질문 게시글 수정
     @Transactional
-    public void modifyQuestionBoard(String title, String content, String userId, Long questionBoardId) {
+    public QuestionBoardDto modifyQuestionBoard(String title, String content, String userId, Long questionBoardId) {
         // user find
         UserAccount userAccount = getUserAccountOrException(userId);
 
         // post exist
+        QuestionBoard questionBoard = getQuestionBoardOrException(questionBoardId);
 
         // post permission
-        
+        if (questionBoard.getUserAccount() != userAccount) {
+                throw new DoubleSApplicationException(
+                        ErrorCode.INVALID_PERMISSION, String.format("%s는 권한이 게시판 번호: '%s' 에 대해서 권한이 없습니다.",
+                        userId, 
+                        questionBoardId
+                )
+            );
+        }
+
+        questionBoard.setTitle(title);
+        questionBoard.setContent(content);
+
+        return QuestionBoardDto.fromEntity(questionBoardRepository.saveAndFlush(questionBoard));
     }
 
     private UserAccount getUserAccountOrException(String userId) {
         // find useraccount
         return userAccountRepository.findById(userId).orElseThrow(() ->
-                    new DoubleSApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s를 찾지 못했습니다.", userId))
+                    new DoubleSApplicationException(ErrorCode.USER_NOT_FOUND, String.format("유저 %s를 찾지 못했습니다.", userId))
                 );
+    }
+
+    private QuestionBoard getQuestionBoardOrException(Long questionBoardId) {
+        return questionBoardRepository.findById(questionBoardId).orElseThrow(() ->
+                new DoubleSApplicationException(ErrorCode.POST_NOT_FOUND, String.format("게시글 번호: '%s' 를 찾지 못했습니다.", questionBoardId))
+        );
     }
 }
