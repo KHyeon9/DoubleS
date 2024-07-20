7fb162a9a6880d435802fdb7cd3ed64a92ae49b2
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-20 18:54:30 +0900

    Backend: 질문 게시판 컨트롤러 수정 테스트 케이스 작성

--- src/test/java/com/doubles/selfstudy/controller/QuestionBoardControllerTest.java
+++ src/test/java/com/doubles/selfstudy/controller/QuestionBoardControllerTest.java
@@ -1,7 +1,11 @@
 package com.doubles.selfstudy.controller;
 
-import com.doubles.selfstudy.controller.request.QuestionBoardCreaeteRequest;
+import com.doubles.selfstudy.controller.request.QuestionBoardCreateRequest;
+import com.doubles.selfstudy.controller.request.QuestionBoardModifyRequest;
+import com.doubles.selfstudy.dto.post.QuestionBoardDto;
+import com.doubles.selfstudy.exception.DoubleSApplicationException;
 import com.doubles.selfstudy.exception.ErrorCode;
+import com.doubles.selfstudy.fixture.QuestionBoardFixture;
 import com.doubles.selfstudy.service.QuestionBoardService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -14,7 +18,12 @@ import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -40,7 +49,7 @@ class QuestionBoardControllerTest {
         // When&Then
         mockMvc.perform(post("/api/main/post/create")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(new QuestionBoardCreaeteRequest(title, content)))
+                        .content(objectMapper.writeValueAsBytes(new QuestionBoardCreateRequest(title, content)))
                 )
                 .andDo(print())
                 .andExpect(status().isOk());
@@ -48,7 +57,7 @@ class QuestionBoardControllerTest {
 
     @Test
     @WithAnonymousUser
-    void 질문_게시글_작성시_로그인이_않된_경우() throws Exception {
+    void 질문_게시글_작성시_로그인이_않된_경우_에러_발생() throws Exception {
         // Given
         String title = "title";
         String content = "content";
@@ -56,9 +65,87 @@ class QuestionBoardControllerTest {
         // When&Then
         mockMvc.perform(post("/api/main/post/create")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(new QuestionBoardCreaeteRequest(title, content)))
+                        .content(objectMapper.writeValueAsBytes(new QuestionBoardCreateRequest(title, content)))
                 )
                 .andDo(print())
                 .andExpect(status().is(ErrorCode.INVALID_TOKEN.getStatus().value()));
+
+    }
+
+    @Test
+    @WithMockUser
+    void 포스트_수정_성공() throws Exception {
+        // Given
+        String title = "title";
+        String content = "content";
+
+        // When
+        when(questionBoardService.modifyQuestionBoard(eq(title), eq(content), any(), eq(1L)))
+                .thenReturn(QuestionBoardDto.fromEntity(QuestionBoardFixture.get("userId")));
+
+        // Then
+        mockMvc.perform(put("/api/main/post/modify/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new QuestionBoardModifyRequest(title, content)))
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    @WithAnonymousUser
+    void 포스트_수정시_로그인하지_않은_경우_에러_발생() throws Exception {
+        // Given
+        String title = "title";
+        String content = "content";
+
+        // When&Then
+        mockMvc.perform(put("/api/main/post/modify/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new QuestionBoardModifyRequest(title, content)))
+                )
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.INVALID_TOKEN.getStatus().value()));
+    }
+
+    @Test
+    @WithMockUser
+    void 포스트_수정시_본인이_작성한_글이_아닌_경우_에러_발생() throws Exception {
+        // Given
+        String title = "title";
+        String content = "content";
+
+        // When
+        doThrow(new DoubleSApplicationException(ErrorCode.INVALID_PERMISSION))
+                .when(questionBoardService).modifyQuestionBoard(eq(title), eq(content), any(), eq(1L));
+
+        // Then
+        mockMvc.perform(put("/api/main/post/modify/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new QuestionBoardModifyRequest(title, content)))
+                )
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.INVALID_PERMISSION.getStatus().value()));
+    }
+
+    @Test
+    @WithMockUser
+    void 포스트_수정시_작성한_글이_없는_경우_에러_발생() throws Exception {
+        // Given
+        String title = "title";
+        String content = "content";
+
+        // When
+        doThrow(new DoubleSApplicationException(ErrorCode.POST_NOT_FOUND))
+                .when(questionBoardService).modifyQuestionBoard(eq(title), eq(content), any(), eq(1L));
+
+        // Then
+        mockMvc.perform(put("/api/main/post/modify/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new QuestionBoardModifyRequest(title, content)))
+                )
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.POST_NOT_FOUND.getStatus().value()));
     }
 }
