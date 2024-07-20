1f77dcbc851ffcafd9a3254ab90f22d790b21c3d
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-20 19:28:30 +0900

    Backend: 질문 게시판 주소 수정
    좀더 명확히 하기 위해 주소를 question_board로 수정

--- src/main/java/com/doubles/selfstudy/controller/QuestionBoardController.java
+++ src/main/java/com/doubles/selfstudy/controller/QuestionBoardController.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/main/post")
+@RequestMapping("/api/main/question_board")
 @RestController
 public class QuestionBoardController {
 
