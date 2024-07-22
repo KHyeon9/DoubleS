23631a79a60cde6bb5f3b9f8f269c218b8cfda59
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-22 20:25:05 +0900

    Backend: 질문 게시글 좋아요 entity와 repositoey 생성

---
+++ src/main/java/com/doubles/selfstudy/repository/QuestionBoardLikeRepository.java
@@ -0,0 +1,7 @@
+package com.doubles.selfstudy.repository;
+
+import com.doubles.selfstudy.entity.QuestionBoardLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionBoardLikeRepository extends JpaRepository<QuestionBoardLike, Long> {
+}
