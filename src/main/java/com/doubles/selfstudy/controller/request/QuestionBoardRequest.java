90533b0758fde3fcf6bb92b8da8f9ff5a6821cb1
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-23 20:34:19 +0900

    Backend: 필요 없는 요청 및 중복 제거

--- src/main/java/com/doubles/selfstudy/controller/request/QuestionBoardRequest.java
+++ src/main/java/com/doubles/selfstudy/controller/request/QuestionBoardRequest.java
@@ -1,38 +1,12 @@
 package com.doubles.selfstudy.controller.request;
 
-import com.doubles.selfstudy.controller.response.UserResponse;
-import com.doubles.selfstudy.dto.post.QuestionBoardDto;
-import com.doubles.selfstudy.dto.post.QuestionBoardTag;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
 public class QuestionBoardRequest {
 
-    private Long id;
-    private UserResponse user; // 유저 정보
-    private String title; // 제목
-    private String content; // 내용
-    private QuestionBoardTag tag; // 태그
-    private LocalDateTime createdAt; // 생성 일시
-    private String createdBy; // 생성자
-    private LocalDateTime modifiedAt; // 수정 일시
-    private String modifiedBy; // 수정자
-
-    public static QuestionBoardRequest fromQuestionBoardDto(QuestionBoardDto dto) {
-        return new QuestionBoardRequest(
-                dto.getId(),
-                UserResponse.fromUserAccountDto(dto.getUserAccountDto()),
-                dto.getTitle(),
-                dto.getContent(),
-                dto.getTag(),
-                dto.getCreatedAt(),
-                dto.getCreatedBy(),
-                dto.getModifiedAt(),
-                dto.getModifiedBy()
-        );
-    }
+    private String title;
+    private String content;
 }
