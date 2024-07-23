c46f07d2f9a0ef06c03492e4e4006c692e5ab066
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-23 20:36:38 +0900

    Backend: 질문 게시글 댓글 엔티티 및 레포지토리 추가

---
+++ src/main/java/com/doubles/selfstudy/repository/QuestionBoardCommentRepository.java
@@ -0,0 +1,9 @@
+package com.doubles.selfstudy.repository;
+
+import com.doubles.selfstudy.entity.QuestionBoardComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionBoardCommentRepository extends JpaRepository<QuestionBoardComment, Long> {
+}
