c88bc2ff9e0a9f7143072dcb2e7fe597b334b4dd
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-23 19:27:20 +0900

    Backend: 좋아요 레포지토리 추가 및 좋아요 서비스 로직 추가
    count와 delete를 레포지토리 간단하게 작성 및 좋아요와 좋아요 구현 및 갯수 조회 간단하게 구현

--- src/main/java/com/doubles/selfstudy/repository/QuestionBoardLikeRepository.java
+++ src/main/java/com/doubles/selfstudy/repository/QuestionBoardLikeRepository.java
@@ -1,7 +1,21 @@
 package com.doubles.selfstudy.repository;
 
+import com.doubles.selfstudy.entity.QuestionBoard;
 import com.doubles.selfstudy.entity.QuestionBoardLike;
+import com.doubles.selfstudy.entity.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+@Repository
 public interface QuestionBoardLikeRepository extends JpaRepository<QuestionBoardLike, Long> {
+
+    Optional<QuestionBoardLike> findByUserAccountAndQuestionBoard(UserAccount userAccount, QuestionBoard questionBoard);
+    Integer countByQuestionBoard(QuestionBoard questionBoard);
+
+    @Transactional
+    void deleteAllByQuestionBoard(@Param("question_board") QuestionBoard questionBoard);
 }
