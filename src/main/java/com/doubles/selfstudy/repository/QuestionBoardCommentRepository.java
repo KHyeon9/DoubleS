ab579ed5d2e8c93137f32d4cbc9f9db49fe7ad2b
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-25 20:26:21 +0900

    Backend: 질문 게시글 삭제시 관련 repository 수정
    처음에 deleteBy사용했다가 오류가나서 직접 jpql를 사용하는 방식으로 변경

--- src/main/java/com/doubles/selfstudy/repository/QuestionBoardCommentRepository.java
+++ src/main/java/com/doubles/selfstudy/repository/QuestionBoardCommentRepository.java
@@ -5,10 +5,18 @@ import com.doubles.selfstudy.entity.QuestionBoardComment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionBoardCommentRepository extends JpaRepository<QuestionBoardComment, Long> {
 
     Page<QuestionBoardComment> findAllByQuestionBoard(QuestionBoard questionBoard, Pageable pageable);
+
+    Long countByQuestionBoard(QuestionBoard questionBoard);
+
+    @Modifying // db변경을 명시
+    @Query("DELETE FROM QuestionBoardComment qbc WHERE qbc.questionBoard.id = :questionBoardId")
+    void deleteAllByQuestionBoardId(Long questionBoardId);
 }
