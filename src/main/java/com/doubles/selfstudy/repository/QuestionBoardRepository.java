3c0c190eb964b82ec7fc7c49cfad8432a279b0ee
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-25 20:27:38 +0900

    Backend: 게시글 목록을 조회시 사용할 메소드 추가
    게시글 목록 조회시 좋아요 수와 댓글 수를 가져오기 위한 메소드 추가.
    조회시 이상하게 조회되어 서브쿼리 사용

--- src/main/java/com/doubles/selfstudy/repository/QuestionBoardRepository.java
+++ src/main/java/com/doubles/selfstudy/repository/QuestionBoardRepository.java
@@ -5,10 +5,17 @@ import com.doubles.selfstudy.entity.UserAccount;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionBoardRepository extends JpaRepository<QuestionBoard, Long> {
 
     Page<QuestionBoard> findByUserAccount(UserAccount userAccount, Pageable pageable);
+
+    @Query("SELECT q, " +
+            "(SELECT COUNT(qbl) FROM QuestionBoardLike qbl WHERE qbl.questionBoard.id = q.id), " +
+            "(SELECT COUNT(qbc) FROM QuestionBoardComment qbc WHERE qbc.questionBoard.id = q.id) " +
+            "FROM QuestionBoard q")
+    Page<Object[]> findAllByWithLikeCountAndCommentCount(Pageable pageable);
 }
