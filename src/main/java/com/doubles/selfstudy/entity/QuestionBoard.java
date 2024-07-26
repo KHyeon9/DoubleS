2b519e70d3cabfb589453c411f29fcb9d81db932
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-26 17:30:49 +0900

    Backend: 질문 게시글이 insert 될 때, 조회수가 0이 아닌 null이 되는 문제 해결

--- src/main/java/com/doubles/selfstudy/entity/QuestionBoard.java
+++ src/main/java/com/doubles/selfstudy/entity/QuestionBoard.java
@@ -51,10 +51,12 @@ public class QuestionBoard extends AuditingFields {
         return QuestionBoard.of(userAccount, title, content, null);
     }
 
+    @PrePersist
+    public void prePersist() {
+        this.viewCount = 0;
+     }
+
     public void plusViewCount() {
-        if (viewCount == null) {
-            viewCount = 0;
-        }
         this.viewCount += 1;
     }
 }
