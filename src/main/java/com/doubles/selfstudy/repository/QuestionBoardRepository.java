ee3c2a433dd9d8330eb3f8d54d12123036f2c1ed
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-26 17:43:49 +0900

    Backend: 필요없는 메소드 삭제

--- src/main/java/com/doubles/selfstudy/repository/QuestionBoardRepository.java
+++ src/main/java/com/doubles/selfstudy/repository/QuestionBoardRepository.java
@@ -1,7 +1,6 @@
 package com.doubles.selfstudy.repository;
 
 import com.doubles.selfstudy.entity.QuestionBoard;
-import com.doubles.selfstudy.entity.UserAccount;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,8 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionBoardRepository extends JpaRepository<QuestionBoard, Long> {
-
-    Page<QuestionBoard> findByUserAccount(UserAccount userAccount, Pageable pageable);
 
     @Query("SELECT q, " +
             "(SELECT COUNT(qbl) FROM QuestionBoardLike qbl WHERE qbl.questionBoard.id = q.id), " +
