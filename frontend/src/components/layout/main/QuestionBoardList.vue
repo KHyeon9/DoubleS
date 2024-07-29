534e09e8d6e8859f0a5b36e47f40e0b5e865781a
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-29 23:51:27 +0900

    Frontend: 질문 게시판 페이징 뼈대 추가

--- frontend/src/components/layout/main/QuestionBoardList.vue
+++ frontend/src/components/layout/main/QuestionBoardList.vue
@@ -68,8 +68,16 @@
                   </router-link>
                 </li>
               </ul>
+              <nav class="mt-4 nav nav-bar d-flex justify-content-end" aria-label="Page navigation">
+                <ul class="pagination">
+                  <li class="page-item"><a class="page-link" href="#">‹</a></li>
+                  <li class="page-item active"><a class="page-link " href="#" >1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item"><a class="page-link" href="#">›</a></li>
+                </ul>
+              </nav>
             </div>
-            페이징
           </div>
         </div>
       </div>
@@ -85,4 +93,9 @@
   .board_info {
     display: flex;
   }
+
+  .active a {
+    color: white;
+  }
+
 </style>
