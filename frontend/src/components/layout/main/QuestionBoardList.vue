a88ee8aa64368f9ed0cae3cc7684fee287c7fb11
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-07-29 21:00:56 +0900

    Frontend: 질문 게시판 상세에 아이콘등 수정 및 리스트 간단하게 작성
    리스트는 대충 뼈대 만드는 식으로 해두었음
    
    TODO: 페이징 할 수 있는 아래 숫자들 작업 고민해 봐야함

--- frontend/src/components/layout/main/QuestionBoardList.vue
+++ frontend/src/components/layout/main/QuestionBoardList.vue
@@ -1,8 +1,80 @@
 <template>
-  <div>
-    question_board
-    <router-link to="/main/question_board/detail">detail</router-link>
-  </div>
+  <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ps ps--active-y">
+    <div class="container-fluid py-4">
+      <div class="d-sm-flex justify-content-between">
+        <div>
+          <a href="javascript:;" class="btn btn-icon bg-gradient-primary">
+            New Post
+          </a>
+        </div>
+        <div class="d-flex">
+          <div class="dropdown d-inline">
+            <a href="javascript:;" class="btn btn-outline-dark dropdown-toggle" data-bs-toggle="dropdown" id="navbarDropdownMenuLink2" aria-expanded="false">
+            Filters
+            </a>
+            <ul class="dropdown-menu dropdown-menu-lg-start px-2 py-3" aria-labelledby="navbarDropdownMenuLink2" style="">
+              <li><a class="dropdown-item border-radius-md" href="javascript:;">Status: Paid</a></li>
+              <li><a class="dropdown-item border-radius-md" href="javascript:;">Status: Refunded</a></li>
+              <li><a class="dropdown-item border-radius-md" href="javascript:;">Status: Canceled</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-header">
+              <h5 class="mb-0">질문 게시판</h5>
+              <p class="text-sm mb-0">
+                궁금한 것을 공유해보세요.
+              </p>
+            </div>
+            <div class="p-3">
+              <ul class="list-group">
+                <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
+                  <router-link class="col-12" to="/main/question_board/detail">
+                    <div class="d-flex flex-column">
+                        <div class="fw-bolder mb-3 fs-5">Title</div>
+                        <span class="mb-2 text-s">content content content content content content content content</span>
+                        <div class="ms-auto board_info">
+                          <div class="text-dark px-3 mb-0"><i class="material-icons text-sm me-2">thumb_up</i><span class="font-weight-bold">13</span></div>
+                          <div class="text-dark px-3 mb-0"><i class="material-icons text-sm me-2">mode_comment</i><span class="font-weight-bold">3</span></div>
+                        </div>
+                    </div>
+                  </router-link>
+                </li>
+                <li class="list-group-item border-0 d-flex p-4 mb-2 mt-3 bg-gray-100 border-radius-lg">
+                  <router-link class="col-12" to="/main/question_board/detail">
+                    <div class="d-flex flex-column">
+                        <div class="fw-bolder mb-3 fs-5">Title2</div>
+                        <span class="mb-2 text-s">content content content content content content content content</span>
+                        <div class="ms-auto board_info">
+                          <div class="text-dark px-3 mb-0"><i class="material-icons text-sm me-2">thumb_up</i><span class="font-weight-bold">0</span></div>
+                          <div class="text-dark px-3 mb-0"><i class="material-icons text-sm me-2">mode_comment</i><span class="font-weight-bold">3</span></div>
+                        </div>
+                    </div>
+                  </router-link>
+                </li>
+                <li class="list-group-item border-0 d-flex p-4 mb-2 mt-3 bg-gray-100 border-radius-lg">
+                  <router-link class="col-12" to="/main/question_board/detail">
+                    <div class="d-flex flex-column">
+                        <div class="fw-bolder mb-3 fs-5">Title3</div>
+                        <span class="mb-2 text-s">content content content content content content content content</span>
+                        <div class="ms-auto board_info">
+                          <div class="text-dark px-3 mb-0"><i class="material-icons text-sm me-2">thumb_up</i><span class="font-weight-bold">9</span></div>
+                          <div class="text-dark px-3 mb-0"><i class="material-icons text-sm me-2">mode_comment</i><span class="font-weight-bold">2</span></div>
+                        </div>
+                    </div>
+                  </router-link>
+                </li>
+              </ul>
+            </div>
+            페이징
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 <script setup>
   import { useIndexStore } from '../../../store/IndexStore';
@@ -10,5 +82,7 @@
   const store = useIndexStore();
 </script>
 <style scoped>
-  
+  .board_info {
+    display: flex;
+  }
 </style>
