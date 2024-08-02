b52a9479f21a3addfa050da2c2c27d1f8d7e0821
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-08-02 20:01:36 +0900

    Frontend: 인증 할 때, 어차피 main으로 들어가야 토큰을 사용하므로 baseURL 수정

--- frontend/src/config/authConfig.js
+++ frontend/src/config/authConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "../store/authStore";
 
 const apiClient = axios.create({
-  baseURL: '/api',
+  baseURL: '/api/main',
   headers: {
     'Content-Type': 'application/json',
   },
