d32a39c59d0b41c2d9c1543beca1566048f9d1b1
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-08-07 19:19:41 +0900

    Frontend: 권한과 topbar에 사용할 nickname 그리고 혹시몰라서 userId도 저장하기 위한 state 작성

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -3,6 +3,9 @@ import { defineStore } from "pinia";
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     token: localStorage.getItem('token') || '',
+    userId: '',
+    nickname: '',
+    roleType: '',
   }),
   actions: {
     setToken(token) {
@@ -12,9 +15,20 @@ export const useAuthStore = defineStore('auth', {
     clearToken() {
       this.token = '';
       localStorage.removeItem('token');
-    }
+    },
+    setUserInfo(userInfo) {
+      this.userId = userInfo.userId;
+      this.nickname = userInfo.nickname;
+      this.roleType = userInfo.roleType;
+    },
+    clearUserInfo() {
+      this.userId = '';
+      this.nickname = '';
+      this.roleType = '';
+    } 
   },
   getters: {
     isAuthenticated: (state) => !!state.token,
+    isAdmin: (state) => state.roleType === 'ADMIN',
   }
 });
