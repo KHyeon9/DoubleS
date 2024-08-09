db3b45df2732889b88f8be833bfdae9db0d72a30
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-08-09 20:38:36 +0900

    Frontend: 질문 게시글 상세에 삭제 api 연결 및 변수명 수정, 버튼 유무 처리
    특정 버튼들이 작성자면 보이고 아니면 보이지 않도록 수정

--- frontend/src/components/layout/main/QuestionBoardDetail.vue
+++ frontend/src/components/layout/main/QuestionBoardDetail.vue
@@ -5,27 +5,33 @@
         <div class="col-auto my-auto">
           <div class="h-100 ms-4">
             <h3 class="mt-2 mb-2">
-              {{ board.title }}
+              {{ questionBoard.title }}
             </h3>
             <p class="mb-0 font-weight-normal text-s">
-              {{ board.user.userId }}
+              {{ questionBoard.user.userId }}
             </p>
             <p class="mb-0 font-weight-normal text-sm">
-              {{ formatDate(board.createdAt) }}
+              {{ formatDate(questionBoard.createdAt) }}
             </p>
           </div>
         </div>
         <div class="col-lg-3 col-md-4 my-sm-auto ms-sm-auto me-sm-0 mx-auto mt-3">
           <div class="nav-wrapper position-relative end-0">
             <ul class="nav nav-pills nav-fill p-1" >
-              <li class="nav-item" role="presentation">
-                <a class="nav-link mb-0 px-0 py-1 " data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="false" tabindex="-1">
+              <li v-if="userId == questionBoard.user.userId" class="nav-item" role="presentation">
+                <router-link :to="`/main/question_board/modify/${questionBoard.id}`"  class="nav-link mb-0 px-0 py-1 " role="tab">
                   <i class="material-icons text-lg position-relative">edit</i>
                   <span class="ms-1">Edit</span>
-                </a>
+                </router-link>
               </li>
-              <li class="nav-item" role="presentation">
-                <a class="nav-link mb-0 px-0 py-1 " href="javascript:;">
+              <li v-if="userId == questionBoard.user.userId" class="nav-item" role="presentation">
+                <button @click="deleteQuestionBoard" class="nav-link mb-0 px-0 py-1" role="tab">
+                  <i class="material-icons text-lg position-relative">delete</i>
+                  <span class="ms-1">Delete</span>
+                </button>
+              </li>
+              <li v-if="userId != questionBoard.user.userId" class="nav-item" role="presentation">
+                <a class="nav-link mb-0 px-0 py-1" role="tab">
                   <i class="material-icons text-lg position-relative">email</i>
                   <span class="ms-1">Messages</span>
                 </a>
@@ -41,22 +47,22 @@
               <hr class="dark horizontal">
               <div class="card-body pt-3">
                 <p class="postContent mb-4">
-                  {{ board.content }}
+                  {{ questionBoard.content }}
                 </p>
                 <div class="row align-items-center px-2 mt-4 mb-2">
                   <div class="col-sm-6">
                     <div class="d-flex">
                       <div class="d-flex align-items-center">
                         <i class="material-icons text-sm me-1 cursor-pointer">visibility</i>
-                        <span class="text-sm me-3 ">{{ board.viewCounts }}</span>
+                        <span class="text-sm me-3 ">{{ questionBoard.viewCounts }}</span>
                       </div>
                       <div class="d-flex align-items-center">
                         <i class="material-icons text-sm me-1 cursor-pointer">thumb_up</i>
-                        <span class="text-sm me-3 ">{{ board.likes }}</span>
+                        <span class="text-sm me-3 ">{{ questionBoard.likes }}</span>
                       </div>
                       <div class="d-flex align-items-center">
                         <i class="material-icons text-sm me-1 cursor-pointer">mode_comment</i>
-                        <span class="text-sm me-3">{{ board.comments }}</span>
+                        <span class="text-sm me-3">{{ questionBoard.comments }}</span>
                       </div>
                     </div>
                   </div>
@@ -73,13 +79,13 @@
                       <h6 class="h5 mt-0">Michael Lewis</h6>
                       <p class="text-s">I always felt like I could do anything. That’s the main thing people are controlled by! Thoughts- their perception of themselves!</p>
                       <p class="text-xs text-end me-4">DateTime</p>
-                      <div class="ms-auto text-end" data-v-4fa5880f="">
-                        <a class="btn btn-link text-danger text-gradient px-3 mb-0" href="javascript:;" data-v-4fa5880f="">
-                          <i class="material-icons text-sm me-2" data-v-4fa5880f="">delete</i>
+                      <div class="ms-auto text-end">
+                        <a  class="btn btn-link text-danger text-gradient px-3 mb-0">
+                          <i class="material-icons text-sm me-2" >delete</i>
                           Delete
                         </a>
-                        <a class="btn btn-link text-dark px-3 mb-0" href="javascript:;" data-v-4fa5880f="">
-                          <i class="material-icons text-sm me-2" data-v-4fa5880f="">edit</i>
+                        <a class="btn btn-link text-dark px-3 mb-0" href="javascript:;" >
+                          <i class="material-icons text-sm me-2">edit</i>
                           Edit
                         </a>
                       </div>
@@ -104,22 +110,41 @@
 </template>
 <script setup>
   import { ref, onMounted } from 'vue';
-  import { useRoute } from 'vue-router';
+  import { useRoute, useRouter } from 'vue-router';
+  import { useAuthStore } from '../../../store/authStore';
+  import { computed } from 'vue';
   import apiClient from '../../../config/authConfig';
   import moment from 'moment';
 
   const route = useRoute();
-  const board = ref({});
+  const router = useRouter();
+  const authStore = useAuthStore();
+  const questionBoard = ref({});
+  const userId = computed(() => authStore.userId)
 
   const getBoardData = async (id) => {
     try {
       const response = await apiClient.get(`/main/question_board/${id}`);
-      board.value = response.data.result
+      questionBoard.value = response.data.result
 
-      console.log( board.value);
+      console.log( questionBoard.value);
     } catch (error) {
       console.log('에러가 발생했습니다.', error);
       alert('질문 게시글을 가져오지 못했습니다.');
+    }
+  };
+
+  const deleteQuestionBoard = async () => {
+    try {
+      if (confirm('정말 삭제하시겠습니까?')) {
+        const response = await apiClient.delete(`/main/question_board/${questionBoard.value.id}`);
+        console.log(response);
+
+        router.push('/main/question_board');
+      }
+    } catch (error) {
+      console.log('에러가 발생했습니다. ', error);
+      alert('질문 게시글을 삭제하는데 실패했습니다.');
     }
   };
 
