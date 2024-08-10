94ee51e072ba411c3cfe2e43a87a2101fd730182
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-08-10 18:42:07 +0900

    Frontend: 댓글 삭제, 수정 기능 api 연결 및 프론트 작업
    댓글 삭제 api 와 연결 및 버튼 등을 생성하여 연결

--- frontend/src/components/layout/main/QuestionBoardDetail.vue
+++ frontend/src/components/layout/main/QuestionBoardDetail.vue
@@ -4,7 +4,11 @@
       <div class="row gx-4 mb-2">
         <div class="col-auto my-auto">
           <div class="h-100 ms-4">
-            <h3 class="mt-2 mb-2">
+            <div class="mt-2">
+              <i class="material-icons text-lg me-2">sell</i>
+              <span class="text-lg me-3 ">{{ questionBoard.tag.value }}</span>
+            </div>
+            <h3 class="mb-2">
               {{ questionBoard.title }}
             </h3>
             <p class="mb-0 font-weight-normal text-s">
@@ -50,54 +54,103 @@
                   {{ questionBoard.content }}
                 </p>
                 <div class="row align-items-center px-2 mt-4 mb-2">
-                  <div class="col-sm-6">
+                  <div class="col-sm-6 px-0">
                     <div class="d-flex">
-                      <div class="d-flex align-items-center">
-                        <i class="material-icons text-sm me-1 cursor-pointer">visibility</i>
-                        <span class="text-sm me-3 ">{{ questionBoard.viewCounts }}</span>
+                      <div class="d-flex align-items-center me-3">
+                        <i class="material-icons text-md me-2">visibility</i>
+                        <span class="text-md">{{ questionBoard.viewCounts }}</span>
                       </div>
-                      <div class="d-flex align-items-center">
-                        <i class="material-icons text-sm me-1 cursor-pointer">thumb_up</i>
-                        <span class="text-sm me-3 ">{{ questionBoard.likes }}</span>
+                      <div class="d-flex align-items-center me-3">
+                        <i class="material-icons text-md me-2">mode_comment</i>
+                        <span class="text-md">{{ questionBoard.comments }}</span>
                       </div>
-                      <div class="d-flex align-items-center">
-                        <i class="material-icons text-sm me-1 cursor-pointer">mode_comment</i>
-                        <span class="text-sm me-3">{{ questionBoard.comments }}</span>
+                      <div class="d-flex align-items-center me-3">
+                        <i class="material-icons text-md me-2 cursor-pointer">thumb_up</i>
+                        <span class="text-md">{{ questionBoard.likes }}</span>
                       </div>
                     </div>
                   </div>
                   <hr class="horizontal dark my-3">
                 </div>
                 <div class="mb-1">
-                  <div class="d-flex mt-3">
+                  <div v-for="quesionBoardComment in questionBoardComments" class="d-flex mt-3">
                     <div class="flex-shrink-0">
                       <i class="material-icons cursor-pointer">
                         person
                       </i>
                     </div>
                     <div class="flex-grow-1 ms-3">
-                      <h6 class="h5 mt-0">Michael Lewis</h6>
-                      <p class="text-s">I always felt like I could do anything. That’s the main thing people are controlled by! Thoughts- their perception of themselves!</p>
-                      <p class="text-xs text-end me-4">DateTime</p>
-                      <div class="ms-auto text-end">
-                        <a  class="btn btn-link text-danger text-gradient px-3 mb-0">
-                          <i class="material-icons text-sm me-2" >delete</i>
-                          Delete
-                        </a>
-                        <a class="btn btn-link text-dark px-3 mb-0" href="javascript:;" >
-                          <i class="material-icons text-sm me-2">edit</i>
-                          Edit
-                        </a>
+                      <h6 class="h5 mt-0">{{ quesionBoardComment.user.userId }}</h6>
+                      <!-- 댓글 내용이 수정 중이면 textarea로 변경 -->
+                      <div v-if="editingCommentId === quesionBoardComment.id">
+                        <div class="flex-grow-1 my-auto">
+                          <div class="input-group input-group-static">
+                            <textarea  v-model="editingCommentText" class="form-control" placeholder="Write your comment" rows="3" spellcheck="false"></textarea>
+                          </div>
+                        </div>
+                        <div class="ms-auto text-end">
+                          <button @click="modifyComment(quesionBoardComment.id)" class="btn btn-link text-dark px-3 mb-0">
+                            <i class="material-icons text-sm me-2">edit</i>
+                            Update
+                          </button>
+                          <button @click="cancelEdit" class="btn btn-link text-danger text-gradient px-3 mb-0">
+                            <i class="material-icons text-sm me-2">cancel</i>
+                            Cancel
+                          </button>
+                        </div>
+                      </div>
+                      <div v-else>
+                        <p class="text-s">{{ quesionBoardComment.comment }}</p>
+                        <p class="text-xs text-end me-4">{{ formatDate(quesionBoardComment.createdAt) }}</p>
+                        <p v-if="quesionBoardComment.createdAt != quesionBoardComment.modifiedAt" 
+                            class="text-xs text-end me-4">수정됨</p>
+                        <div v-if="quesionBoardComment.user.userId == userId" class="ms-auto text-end">
+                          <button  @click="editComment(quesionBoardComment.id, quesionBoardComment.comment)" 
+                                    class="btn btn-link text-dark px-3 mb-0">
+                            <i class="material-icons text-sm me-2">edit</i>
+                            Edit
+                          </button>
+                          <button  @click="deleteComment(quesionBoardComment.id)"
+                                  class="btn btn-link text-danger text-gradient px-3 mb-0"
+                          >
+                            <i class="material-icons text-sm me-2" >delete</i>
+                            Delete
+                          </button>
+                        </div>
                       </div>
                     </div>
                   </div>
+                  <!-- 페이징 -->
+                  <nav v-if="totalPages > 1" class="mt-4 nav nav-bar d-flex justify-content-end" aria-label="Page navigation">
+                    <ul class="pagination">
+                      <li class="page-item" :class="{ disabled: page === 0}">
+                        <a class="page-link" @click.prevent="prevPageAndFetch">‹</a>
+                      </li>
+                      <li 
+                        v-for="pageNumber in paginatedPageNumbers"
+                        :key="pageNumber"
+                        class="page-item"
+                        :class="{ active: pageNumber - 1 === page }"
+                      >
+                        <a class="page-link " @click.prevent="goToPageAndFetch(pageNumber - 1)">{{ pageNumber }}</a>
+                      </li>
+                      <li class="page-item" :class="{ disabled: page === totalPages - 1 }">
+                        <a class="page-link" @click.prevent="nextPageAndFetch">›</a>
+                      </li>
+                    </ul>
+                  </nav>
                   <div class="d-flex mt-4">
                     <div class="flex-grow-1 my-auto">
                       <div class="input-group input-group-static">
-                        <textarea class="form-control" placeholder="Write your comment" rows="4" spellcheck="false"></textarea>
+                        <textarea v-model="quetionBoardCommentText" class="form-control" placeholder="Write your comment" rows="4" spellcheck="false"></textarea>
                       </div>
                     </div>
-                    <button class="btn bg-gradient-primary btn-sm mt-auto mb-0 ms-2" type="button" name="button"><i class="material-icons text-sm">send</i></button>
+                    <button @click="createQuestionBoardComment" 
+                            class="btn bg-gradient-primary btn-sm mt-auto mb-0 ms-2" 
+                            type="button" name="button"
+                    >
+                      <i class="material-icons text-sm">send</i>
+                    </button>
                   </div>
                 </div>
               </div>
@@ -113,19 +166,33 @@
   import { useRoute, useRouter } from 'vue-router';
   import { useAuthStore } from '../../../store/authStore';
   import { computed } from 'vue';
+  import { usePagination } from '../../../utils/pagination';
   import apiClient from '../../../config/authConfig';
-  import moment from 'moment';
 
   const route = useRoute();
   const router = useRouter();
   const authStore = useAuthStore();
   const questionBoard = ref({});
   const userId = computed(() => authStore.userId)
+  const questionBoardComments = ref([]);
+  const quetionBoardCommentText = ref('');
+  const editingCommentId = ref(null);
+  const editingCommentText = ref('');
+  const totalElememts = ref(0);
+  const { 
+    page, 
+    totalPages, 
+    nextPage, 
+    prevPage, 
+    goToPage, 
+    formatDate, 
+    paginatedPageNumbers 
+  } = usePagination();
 
-  const getBoardData = async (id) => {
+  const getQuestionBoard = async (id) => {
     try {
       const response = await apiClient.get(`/main/question_board/${id}`);
-      questionBoard.value = response.data.result
+      questionBoard.value = response.data.result;
 
       console.log( questionBoard.value);
     } catch (error) {
@@ -133,6 +200,25 @@
       alert('질문 게시글을 가져오지 못했습니다.');
     }
   };
+
+  const getQuestionBoardComment = async (id) => {
+    try {
+      const response = await apiClient.get(`/main/question_board/${id}/comment`, {
+        params: {
+          page: page.value,
+        },
+      });
+
+      questionBoardComments.value = response.data.result.content;
+      totalPages.value = response.data.result.totalPages;
+      totalElememts.value = response.data.result.totalElements;
+
+      console.log(response.data);
+    } catch (error) {
+      console.log('에러가 발생했습니다.', error);
+      alert('해당 게시글의 댓글을 가져오지 못했습니다.');
+    }
+  }
 
   const deleteQuestionBoard = async () => {
     try {
@@ -148,18 +234,113 @@
     }
   };
 
-  const formatDate = (date) => {
-    return moment(date).format('YYYY/MM/DD');
+  const createQuestionBoardComment = async () => {
+    if (!quetionBoardCommentText.value) {
+      alert('댓글이 입력되지 않았습니다.');
+      return;
+    }
+
+    try {
+      const response = await apiClient.post(`/main/question_board/${questionBoard.value.id}/comment`, {
+        comment: quetionBoardCommentText.value,
+      });
+
+      console.log(response.data);
+
+      quetionBoardCommentText.value = '';
+
+      alert('댓글이 생성되었습니다.');
+
+      // 최신 페이지로 이동
+      page.value = totalPages.value;
+      if (totalElememts.value % 10 != 0) {
+        page.value -= 1;
+      }
+      await getQuestionBoardComment(questionBoard.value.id);
+    } catch (error) {
+      console.log('에러가 발생했습니다. ', error);
+      alert('댓글 생성에 실패했습니다.');
+    }
+  };
+
+  const modifyComment = async (questiionBoardCommentId) => {
+    if (!editingCommentText.value) {
+      alert('수정될 댓글이 입력되지 않았습니다.');
+      return;
+    }
+
+    try {
+      const response = await apiClient.put(`/main/question_board/${questionBoard.value.id}/comment/${questiionBoardCommentId}`, {
+        comment: editingCommentText.value,
+      });
+
+      // 수정 완료 후 댓글 업데이트
+      await getQuestionBoardComment(questionBoard.value.id);
+
+      // 수정 상태 초기화
+      cancelEdit();
+
+      alert('댓글이 수정되었습니다.');
+
+    } catch (error) {
+      console.log('에러가 발생했습니다. ', error);
+      alert('댓글 수정에 실패했습니다.');
+    }
+  };
+
+  const deleteComment = async (questiionBoardCommentId) => {
+    try {
+      if (confirm('정말 댓글을 삭제하시겠습니까?')) {
+        const response = await apiClient.delete(`/main/question_board/${questionBoard.value.id}/comment/${questiionBoardCommentId}`);
+
+        await getQuestionBoardComment(questionBoard.value.id);
+
+        alert('댓글이 삭제되었습니다.')
+      }
+    } catch (error) {
+      console.log('에러가 발생했습니다. ', error);
+      alert('댓글 삭제에 실패했습니다.');
+    }
+  };
+
+  const editComment = (commentId, commentText) => {
+    editingCommentId.value = commentId;
+    editingCommentText.value = commentText;
+  };
+
+  const cancelEdit = () => {
+    editingCommentId.value = null;
+    editingCommentText.value = '';
+  };
+
+  const nextPageAndFetch = () => {
+    nextPage();
+    getQuestionBoardComment(questionBoard.value.id);
+  };
+
+  const prevPageAndFetch = () => {
+    prevPage();
+    getQuestionBoardComment(questionBoard.value.id);
+  };
+
+  const goToPageAndFetch = (pageNumber) => {
+    goToPage(pageNumber);
+    getQuestionBoardComment(questionBoard.value.id);
   };
 
 
   onMounted(() => {
     const boardId = route.params.id;
-    getBoardData(boardId);
+    getQuestionBoard(boardId);
+    getQuestionBoardComment(boardId);
   });
 </script>
 <style scoped>
   .postContent {
     font-size: 19px;
+  }
+
+  .active a {
+    color: white;
   }
 </style>
