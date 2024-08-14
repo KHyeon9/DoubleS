b056b508d2259c4e1bda2f2f225f84d7a718333a
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-08-15 00:46:00 +0900

    Backend: 유저 정보 수정 컨트롤러 로직 추가 및 테스트 완료

--- src/main/java/com/doubles/selfstudy/controller/UserController.java
+++ src/main/java/com/doubles/selfstudy/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.doubles.selfstudy.controller;
 
 import com.doubles.selfstudy.controller.request.UserLoginRequest;
+import com.doubles.selfstudy.controller.request.UserModifyRequest;
 import com.doubles.selfstudy.controller.request.UserRegistRequest;
 import com.doubles.selfstudy.controller.response.*;
 import com.doubles.selfstudy.dto.question.QuestionBoardDto;
@@ -38,11 +39,21 @@ public class UserController {
         return Response.success(UserRegistResponse.fromDto(userAccountDto));
     }
 
+    // user login시 정보 조회
     @PostMapping("/user_info")
     public Response<LoginUserInfoResponse> getLoginUserInfo(@RequestBody UserLoginRequest request) {
         UserAccountDto userAccountDto = userAccountService.getUserInfo(request.getUserId());
 
         return Response.success(LoginUserInfoResponse.fromUserAccountDto(userAccountDto));
+    }
+
+    // user 프로필에서 정보 조회
+    @GetMapping("/main/user_info")
+    public Response<ProfileResponse> getLoginUserInfo(Authentication authentication) {
+
+        return Response.success(ProfileResponse
+                .fromUserAccountDto(userAccountService.getUserInfo(authentication.getName()))
+        );
     }
 
     @GetMapping("/main/profile")
@@ -60,6 +71,23 @@ public class UserController {
                         userAccountDto,
                         questionBoardDtoList
                 ));
+    }
+    @PutMapping("/main/profile")
+    public Response<ProfileResponse> modifyUserInfo(
+            Authentication authentication,
+            @RequestBody UserModifyRequest request
+            ) {
+        return Response.success(
+                ProfileResponse.fromUserAccountDto(
+                        userAccountService
+                                .modifiyUserInfo(
+                                        authentication.getName(),
+                                        request.getNickname(),
+                                        request.getEmail(),
+                                        request.getMemo()
+                                )
+                    )
+        );
     }
 
     @GetMapping("/alarm")
