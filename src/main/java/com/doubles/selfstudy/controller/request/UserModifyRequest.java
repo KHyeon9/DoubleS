275a7b6c774820ca72a9761389c7204113df775b
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-08-15 00:35:49 +0900

    Backend: 유저 수정시 사용할 요청 생성

---
+++ src/main/java/com/doubles/selfstudy/controller/request/UserModifyRequest.java
@@ -0,0 +1,15 @@
+package com.doubles.selfstudy.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserModifyRequest {
+
+    private String userId;
+    private String nickname;
+    private String email;
+    private String memo;
+    private String password;
+}
