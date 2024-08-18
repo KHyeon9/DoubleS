55a301cd94cc6cfba242e5fb817b3d0e76c4965f
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-08-18 20:58:21 +0900

    Backend: 메소드 명 변경 및 todo 완료 횟수 메소드 추가

--- src/main/java/com/doubles/selfstudy/repository/TodoRepository.java
+++ src/main/java/com/doubles/selfstudy/repository/TodoRepository.java
@@ -3,6 +3,8 @@ package com.doubles.selfstudy.repository;
 import com.doubles.selfstudy.entity.Todo;
 import com.doubles.selfstudy.entity.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,8 @@ import java.util.List;
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     List<Todo> findAllByUserAccount(UserAccount userAccount);
+
+    // 특정 사용자의 완료된 todo리스트의 총 갯수 반환
+    @Query("SELECT COUNT(t) FROM Todo t WHERE t.userAccount.userId = :userId AND t.isCompleted = true")
+    Integer countCompletedByUserId(@Param("userId") String userId);
 }
