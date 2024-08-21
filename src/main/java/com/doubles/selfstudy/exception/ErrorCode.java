fde40ec26aa2ee38191e18a750b5fa5c608528bd
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-08-21 20:26:29 +0900

    Backend: study group 부분에서 사용할 에러 코드 추가

--- src/main/java/com/doubles/selfstudy/exception/ErrorCode.java
+++ src/main/java/com/doubles/selfstudy/exception/ErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     DUPLICATED_USER_ID(HttpStatus.CONFLICT, "User ID is duplicated"),
+    DUPLICATED_STUDY_GROUP(HttpStatus.CONFLICT, "Study Group is duplicated"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "token is invalid"),
@@ -15,6 +16,8 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post not founded"),
     TODO_NOT_FOUND(HttpStatus.NOT_FOUND, "Todo not founded"),
+    USER_STUDY_GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "User Study Group not founded"),
+    STUDY_GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "Study Group not founded"),
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "Comment not founded"),
     ALREADY_LIKED(HttpStatus.CONFLICT, "User already liked the post"),
     ACCESS_DENIED(HttpStatus.FORBIDDEN, "Permission is invalid"),
