ca0e00c50e0d815126f20b5e214a868b1554397b
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-08-21 20:23:11 +0900

    Backend: 스터디 그룹 repository 작성

---
+++ src/main/java/com/doubles/selfstudy/repository/UserStudyGroupRepository.java
@@ -0,0 +1,24 @@
+package com.doubles.selfstudy.repository;
+
+import com.doubles.selfstudy.entity.UserAccount;
+import com.doubles.selfstudy.entity.UserStudyGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserStudyGroupRepository extends JpaRepository<UserStudyGroup, Long> {
+
+    Optional<UserStudyGroup> findByUserAccount(UserAccount userAccount);
+    void deleteByUserAccount(UserAccount userAccount);
+
+    @Query("SELECT usg.userAccount " +
+            "FROM UserStudyGroup usg " +
+            "WHERE usg.studyGroup.id = :studyGroupId " +
+            "ORDER BY usg.id DESC")
+    List<UserAccount> findAllByStudyGroupId(@Param("studyGroupId") Long studyGroupId);
+}
