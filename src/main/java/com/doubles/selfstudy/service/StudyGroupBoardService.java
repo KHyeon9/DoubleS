fa4cfb5136db1f3c6279b4b80fd0af5344c50f29
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-08-22 20:31:55 +0900

    Backend: 스터디 그룹 게시판 서비스 로직 작성
    아직 테스트 진행 X
    TODO: 댓글 삭제 부분 추가되야함

---
+++ src/main/java/com/doubles/selfstudy/service/StudyGroupBoardService.java
@@ -0,0 +1,117 @@
+package com.doubles.selfstudy.service;
+
+import com.doubles.selfstudy.dto.studygroup.StudyGroupBoardDto;
+import com.doubles.selfstudy.entity.StudyGroupBoard;
+import com.doubles.selfstudy.entity.UserAccount;
+import com.doubles.selfstudy.entity.UserStudyGroup;
+import com.doubles.selfstudy.exception.DoubleSApplicationException;
+import com.doubles.selfstudy.exception.ErrorCode;
+import com.doubles.selfstudy.repository.StudyGroupBoardRepository;
+import com.doubles.selfstudy.utils.ServiceUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class StudyGroupBoardService {
+
+    private final StudyGroupBoardRepository studyGroupBoardRepository;
+    private final ServiceUtils serviceUtils;
+
+    // 스터디 그룹전용 게시판 리스트 조회
+    public Page<StudyGroupBoardDto> studyGroupBoardList(String userId, Pageable pageable) {
+        // user 확인
+        UserAccount userAccount = serviceUtils.getUserAccountOrException(userId);
+
+        // user study group 확인
+        UserStudyGroup userStudyGroup = serviceUtils.getUserStudyGroupOrException(userAccount);
+
+        return studyGroupBoardRepository
+                .findAllByStudyGroupId(userStudyGroup.getStudyGroup().getId(), pageable)
+                .map(StudyGroupBoardDto::fromEntity);
+    }
+
+    // 스터디 그룹 게시글 상세 조회
+    public StudyGroupBoardDto studyGroupBoardDetail(Long studyGroupBoardId) {
+        // 스터디 그룹 게시판 조회
+        StudyGroupBoard studyGroupBoard = serviceUtils.getStudyGroupBoardOrException(studyGroupBoardId);
+
+        // 댓글 수 조회
+        // Long comments =
+
+        return StudyGroupBoardDto.fromEntity(studyGroupBoard);
+    }
+
+    // 스터디 그룹 게시글 생성
+    @Transactional
+    public void createStudyGroupBoard(String userId, String title, String content) {
+        // user 확인
+        UserAccount userAccount = serviceUtils.getUserAccountOrException(userId);
+
+        // user study group 확인
+        UserStudyGroup userStudyGroup = serviceUtils.getUserStudyGroupOrException(userAccount);
+
+        StudyGroupBoard studyGroupBoard = StudyGroupBoard.of(
+                userAccount,
+                userStudyGroup.getStudyGroup(),
+                title,
+                content
+        );
+
+        studyGroupBoardRepository.save(studyGroupBoard);
+    }
+
+    // 스터디 그룹 게시글 수정
+    @Transactional
+    public StudyGroupBoardDto modifyStudyGroupBoard(String userId, Long studyGroupBoardId, String title, String content) {
+        // user 확인
+        UserAccount userAccount = serviceUtils.getUserAccountOrException(userId);
+
+        // study group board 조회
+        StudyGroupBoard studyGroupBoard = serviceUtils.getStudyGroupBoardOrException(studyGroupBoardId);
+
+        if (studyGroupBoard.getUserAccount() != userAccount) {
+            throw new DoubleSApplicationException(
+                    ErrorCode.INVALID_PERMISSION, String.format(
+                    "%s는 권한이 스터디 게시판 번호: '%s' 에 대해서 권한이 없습니다.",
+                    userId,
+                    studyGroupBoardId
+            )
+            );
+        }
+
+        studyGroupBoard.setTitle(title);
+        studyGroupBoard.setContent(content);
+
+        return StudyGroupBoardDto.fromEntity(
+                studyGroupBoardRepository
+                        .saveAndFlush(studyGroupBoard)
+        );
+    }
+
+    // 스터그 그룹 게시글 삭제
+    @Transactional
+    public void deleteStudyGroupBoard(String userId, Long studyGroupBoardId) {
+        // user 확인
+        UserAccount userAccount = serviceUtils.getUserAccountOrException(userId);
+
+        // study group board 조회
+        StudyGroupBoard studyGroupBoard = serviceUtils.getStudyGroupBoardOrException(studyGroupBoardId);
+
+        if (studyGroupBoard.getUserAccount() != userAccount) {
+            throw new DoubleSApplicationException(
+                    ErrorCode.INVALID_PERMISSION, String.format(
+                    "%s는 권한이 스터디 게시판 번호: '%s' 에 대해서 권한이 없습니다.",
+                    userId,
+                    studyGroupBoardId
+            )
+            );
+        }
+
+        // TODO: 댓글 삭제 부분 추가되야함
+        studyGroupBoardRepository.delete(studyGroupBoard);
+    }
+}
