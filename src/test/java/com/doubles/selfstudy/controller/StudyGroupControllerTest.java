63adcc12af7ee7304d23f4fe9432b0681c0adc96
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-08-28 18:48:42 +0900

    Test: import 정리

--- src/test/java/com/doubles/selfstudy/controller/StudyGroupControllerTest.java
+++ src/test/java/com/doubles/selfstudy/controller/StudyGroupControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
