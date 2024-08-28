464bc46cdf82e0db58ed428548f286692e27fe2c
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-08-28 21:31:40 +0900

    Test: fixture에 필요없는 부분 제거 및 수정, 주석 추가

--- src/test/java/com/doubles/selfstudy/fixture/StudyGroupFixture.java
+++ src/test/java/com/doubles/selfstudy/fixture/StudyGroupFixture.java
@@ -4,11 +4,12 @@ import com.doubles.selfstudy.entity.StudyGroup;
 
 public class StudyGroupFixture {
 
+    // question board와 다르게 연관관계가 복잡하므로 id를 설정해야함
     public static StudyGroup get() {
         return StudyGroup.of(1L, "StudyGroupName", "description");
     }
 
     public static StudyGroup get(String studyGroupName, String description) {
-        return StudyGroup.of(1L, studyGroupName, description);
+        return StudyGroup.of(studyGroupName, description);
     }
 }
