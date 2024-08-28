f9a988f97427267c2a87c25d46b1ddc22d695bdf
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-08-28 20:41:18 +0900

    Test: StudyGroupBoardFixture에 새로운 메소드 작성

--- src/test/java/com/doubles/selfstudy/fixture/StudyGroupBoardFixture.java
+++ src/test/java/com/doubles/selfstudy/fixture/StudyGroupBoardFixture.java
@@ -6,7 +6,14 @@ import com.doubles.selfstudy.entity.UserAccount;
 
 public class StudyGroupBoardFixture {
 
+    public static StudyGroupBoard get(String userId, String title, String content) {
+        UserAccount userAccount = UserAccount.of(userId, "test", "test", "test", null);
+        StudyGroup studyGroup = StudyGroup.of("test", "test");
+
+        return StudyGroupBoard.of(userAccount, studyGroup, title, content);
+    }
+
     public static StudyGroupBoard get(UserAccount userAccount, StudyGroup studyGroup, String title, String content) {
-        return StudyGroupBoard.of(1L, userAccount, studyGroup, title, content);
+        return StudyGroupBoard.of(userAccount, studyGroup, title, content);
     }
 }
