b86e3f54aea3b092152c8f8cf2cd881a91b9411e
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-09-06 21:10:12 +0900

    Backend: web socket config 작성
    아직 직접 사용을 안해봐서 오류가 생기면 수정해야 할 수도 있음

---
+++ src/main/java/com/doubles/selfstudy/config/WebSocketBrokerConfig.java
@@ -0,0 +1,33 @@
+package com.doubles.selfstudy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@EnableWebSocketMessageBroker
+@Configuration
+public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
+
+    // 메시지를 중간에서 라우팅할 때 사용하는 메시지 브로커를 구성
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 메시지를 전송할 브로커 경로
+        // 클라이언트에서 1번 채널을 구독하고자 할 때는 /sub/1형식과 같은 규칙을 따라야 한다.
+        registry.enableSimpleBroker("/sub");
+
+        // 클라이언트가 메시지를 전송할 경로.
+        // 즉, /pub로 시작하는 메시지만 해당 Broker에서 받아서 처리한다.
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    // 클라이언트에서 WebSocket에 접속할 수 있는 endpoint를 지정
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // 클라이언트가 연결할 엔드포인트
+        registry.addEndpoint("/ws/init")  // ex) ws://localhost:8080/chatting
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
