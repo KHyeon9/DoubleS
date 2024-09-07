32c99599d5f992db7292da866e4c85f5c687d29a
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-09-07 20:25:02 +0900

    Backend: 채팅 서비스 로직 작성
    추가적으로 메소드를 작성해야할 수도 있음

--- src/main/java/com/doubles/selfstudy/service/ChatService.java
+++ src/main/java/com/doubles/selfstudy/service/ChatService.java
@@ -2,43 +2,94 @@ package com.doubles.selfstudy.service;
 
 import com.doubles.selfstudy.dto.Chat.ChatMessageDto;
 import com.doubles.selfstudy.dto.Chat.ChatRoomDto;
+import com.doubles.selfstudy.entity.ChatMessage;
+import com.doubles.selfstudy.entity.ChatRoom;
+import com.doubles.selfstudy.entity.UserAccount;
+import com.doubles.selfstudy.exception.DoubleSApplicationException;
+import com.doubles.selfstudy.exception.ErrorCode;
+import com.doubles.selfstudy.repository.ChatMessageRepository;
 import com.doubles.selfstudy.repository.ChatRoomRepository;
 import com.doubles.selfstudy.utils.ServiceUtils;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class ChatService {
 
     private final ChatRoomRepository chatRoomRepository;
-    private final ObjectMapper objectMapper;
+    private final ChatMessageRepository chatMessageRepository;
     private final ServiceUtils serviceUtils;
 
     // 채팅룸 리스트 조회
     public List<ChatRoomDto> chatRoomList(String userId) {
-        return List.of();
+        // 유저 조회
+        UserAccount userAccount = serviceUtils.getUserAccountOrException(userId);
+
+        // 채팅룸 리스트 조회
+        return chatRoomRepository
+                .findAllChatRoomsByUser(userAccount)
+                .stream()
+                .map(ChatRoomDto::fromEntity)
+                .toList();
     }
 
     // 채팅 메세지 리스트 조회
     public List<ChatMessageDto> chatMessageListByChatRoomId(Long chatRoomId) {
-        return List.of();
+        // 채팅룸 조회
+        ChatRoom chatRoom = serviceUtils.getChatRoomOrException(chatRoomId);
+        
+        // 채팅 내용 조회
+        return chatMessageRepository
+                .findAllByChatRoom(chatRoom)
+                .stream()
+                .map(ChatMessageDto::fromEntity)
+                .toList();
     }
 
     // 채팅룸 생성
     @Transactional
     public void newChatRoom(String fromUserId, String toUserId) {
+        // 채팅룸 생성하는 유저 조회
+        UserAccount user1 = serviceUtils.getUserAccountOrException(fromUserId);
 
+        // 상대 유저 조회
+        UserAccount user2 = serviceUtils.getUserAccountOrException(toUserId);
+
+        // 채팅룸 체크
+        Optional<ChatRoom> chatRoom = chatRoomRepository.findByUsers(user1, user2);
+
+        if (chatRoom.isPresent()) {
+            throw new DoubleSApplicationException(
+                    ErrorCode.CHAT_ROOM_IS_EXIST,
+                    "채팅방이 이미 있습니다."
+            );
+        }
+
+        // 채팅룸 생성
+        ChatRoom newChatRoom = ChatRoom.of(user1, user2);
+        
+        // 저장
+        chatRoomRepository.save(newChatRoom);
     }
 
     // 채팅 메세지 생성
     @Transactional
     public void insertChatMessage(Long chatRoomId, String userId, String message) {
-        
+        // 채팅룸 조회
+        ChatRoom chatRoom = serviceUtils.getChatRoomOrException(chatRoomId);
+
+        // 유저 조회
+        UserAccount userAccount = serviceUtils.getUserAccountOrException(userId);
+
+        // 메세지 생성
+        ChatMessage chatMessage = ChatMessage.of(chatRoom, userAccount, message);
+
+        chatMessageRepository.save(chatMessage);
     }
 
 }
