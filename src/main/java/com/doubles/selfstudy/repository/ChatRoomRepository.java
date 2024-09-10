5b4a8dcb5ae195351f1133d770e3fca4d9bc8916
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-09-10 21:04:31 +0900

    Backend: 채팅방 가져올때 마지막 메세지와 그 메세지 생성 날짜를 가져오기 위한 레포지토리 로직 수정

--- src/main/java/com/doubles/selfstudy/repository/ChatRoomRepository.java
+++ src/main/java/com/doubles/selfstudy/repository/ChatRoomRepository.java
@@ -13,8 +13,14 @@ import java.util.Optional;
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     // 해당 유저가 속한 채팅방 조회
-    @Query("SELECT cr FROM ChatRoom cr JOIN FETCH cr.user1 JOIN FETCH cr.user2 WHERE cr.user1 = :user OR cr.user2 = :user")
-    List<ChatRoom> findAllChatRoomsByUser(@Param("user") UserAccount user);
+    @Query("SELECT DISTINCT cr, cm.message, cm.createdAt " +
+            "FROM ChatRoom cr " +
+            "LEFT JOIN FETCH cr.user1 u1 " +
+            "LEFT JOIN FETCH cr.user2 u2 " +
+            "LEFT JOIN ChatMessage cm ON cm.chatRoom = cr AND cm.createdAt = " +
+            "       (SELECT MAX(cm2.createdAt) FROM ChatMessage cm2 WHERE cm2.chatRoom = cr) " +
+            "WHERE u1 = :user OR u2 = :user")
+    List<Object[]> findAllChatRoomsByUser(@Param("user") UserAccount user);
 
     @Query("SELECT cr FROM ChatRoom cr WHERE (cr.user1 = :user1 AND cr.user2 = :user2) OR (cr.user1 = :user2 AND cr.user2 = :user1)")
     Optional<ChatRoom> findByUsers(@Param("user1") UserAccount user1, @Param("user2") UserAccount user2);
