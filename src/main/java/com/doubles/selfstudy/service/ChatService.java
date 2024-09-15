bcd036df8b4ee05275834678fe9bc14bdaa62ffd
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-09-15 21:20:09 +0900

    Backend: 레포지토리 쿼리 변경으로 인한 서비스 로직 수정

--- src/main/java/com/doubles/selfstudy/service/ChatService.java
+++ src/main/java/com/doubles/selfstudy/service/ChatService.java
@@ -32,7 +32,7 @@ public class ChatService {
         UserAccount userAccount = serviceUtils.getUserAccountOrException(userId);
 
         // 채팅룸 리스트 조회
-        List<Object[]> results = chatRoomRepository.findAllChatRoomsByUser(userAccount);
+        List<Object[]> results = chatRoomRepository.findAllChatRoomsByUser(userAccount, userAccount.getUserId());
 
         return results.stream().map(
                 result -> ChatRoomDto
@@ -50,7 +50,7 @@ public class ChatService {
         UserAccount userAccount = serviceUtils.getUserAccountOrException(userId);
 
         // 채팅룸 리스트를 닉네임으로 검색
-        List<Object[]> results = chatRoomRepository.findAllChatRoomsByUserAndNickname(userAccount, nickname);
+        List<Object[]> results = chatRoomRepository.findAllChatRoomsByUserAndNickname(userAccount, userAccount.getUserId(), nickname);
 
         return results.stream().map(
                 result -> ChatRoomDto
@@ -125,24 +125,23 @@ public class ChatService {
         UserAccount userAccount = serviceUtils.getUserAccountOrException(userId);
 
         ChatMessage message = null;
-
-        // id 제거
-        if (chatRoom.getUser1() == userAccount) {
-            chatRoom.setUser1(null);
+        
+        if (chatRoom.getLeaveUserId() != null) {
+            // 채팅방에 남은 사람이 한명이면 채팅방 삭제
+            chatMessageRepository.deleteAllByChatRoom(chatRoom);
+            chatRoomRepository.delete(chatRoom);
+        } else if (chatRoom.getUser1() == userAccount) {
+            // 유저1의 탈퇴시 상황
+            chatRoom.setLeaveUserId(chatRoom.getUser1().getUserId());
             message = ChatMessage.of(chatRoom, userAccount, userAccount.getNickname() + "이 채팅방을 나갔습니다.");
         } else if (chatRoom.getUser2() == userAccount) {
-            chatRoom.setUser2(null);
+            // 유저2의 탈퇴시 상황
+            chatRoom.setLeaveUserId(chatRoom.getUser2().getUserId());
             message = ChatMessage.of(chatRoom, userAccount, userAccount.getNickname() + "이 채팅방을 나갔습니다.");
         }
-
+        
         if (message != null) {
             chatMessageRepository.save(message);
-        }
-
-        // 아이디 2개다 null이면 채팅방 삭제
-        if (chatRoom.getUser1() == null && chatRoom.getUser2() == null) {
-            chatMessageRepository.findAllByChatRoom(chatRoom);
-            chatRoomRepository.delete(chatRoom);
         }
     }
 
