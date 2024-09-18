6e025c0dff9c9f80dc6e704aa1bce33e31479c6f
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-09-18 19:32:16 +0900

    Frontend: 알람 디자인 및 페이징 뼈대 추가

--- frontend/src/components/layout/main/Alarm.vue
+++ frontend/src/components/layout/main/Alarm.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
+        <div class="col-lg-10 col-md-10 mx-auto">
           <div class="card mt-4">
             <div class="card-header p-3">
-              <h5 class="mb-0">Alerts</h5>
+              <h5 class="mb-0">Alarm List</h5>
             </div>
-            <div class="card-body p-3 pb-0">
+            <div class="alarm-body card-body p-3 pb-0">
               <div class="alert alert-primary alert-dismissible text-white" role="alert">
                 <span class="text-sm">A simple primary alert with <a href="javascript:;" class="alert-link text-white">an example link</a>. Give it a click if you like.</span>
                 <button type="button" class="btn-close text-lg py-3 opacity-10" data-bs-dismiss="alert" aria-label="Close">
@@ -14,6 +14,12 @@
               </div>
               <div class="alert alert-secondary alert-dismissible text-white" role="alert">
                 <span class="text-sm">A simple secondary alert with <a href="javascript:;" class="alert-link text-white">an example link</a>. Give it a click if you like.</span>
+                <button type="button" class="btn-close text-lg py-3 opacity-10" data-bs-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="alert alert-primary alert-dismissible text-white" role="alert">
+                <span class="text-sm">A simple primary alert with <a href="javascript:;" class="alert-link text-white">an example link</a>. Give it a click if you like.</span>
                 <button type="button" class="btn-close text-lg py-3 opacity-10" data-bs-dismiss="alert" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
@@ -42,6 +48,12 @@
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
+              <div class="alert alert-warning alert-dismissible text-white" role="alert">
+                <span class="text-sm">A simple warning alert with <a href="javascript:;" class="alert-link text-white">an example link</a>. Give it a click if you like.</span>
+                <button type="button" class="btn-close text-lg py-3 opacity-10" data-bs-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
               <div class="alert alert-light alert-dismissible text-white" role="alert">
                 <span class="text-sm">A simple light alert with <a href="javascript:;" class="alert-link text-white">an example link</a>. Give it a click if you like.</span>
                 <button type="button" class="btn-close text-lg py-3 opacity-10" data-bs-dismiss="alert" aria-label="Close">
@@ -54,7 +66,27 @@
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
+              
             </div>
+            <!-- 페이징 -->
+            <nav class="mt-4 nav nav-bar d-flex justify-content-end" aria-label="Page navigation">
+              <ul class="pagination">
+                <li class="page-item" :class="{ disabled: page === 0}">
+                  <a class="page-link" @click.prevent="prevPageAndFetch">‹</a>
+                </li>
+                <li 
+                  v-for="pageNumber in paginatedPageNumbers"
+                  :key="pageNumber"
+                  class="page-item"
+                  :class="{ active: pageNumber - 1 === page }"
+                >
+                  <a class="page-link " @click.prevent="goToPageAndFetch(pageNumber - 1)">{{ pageNumber }}</a>
+                </li>
+                <li class="page-item" :class="{ disabled: page === totalPages - 1 }">
+                  <a class="page-link" @click.prevent="nextPageAndFetch">›</a>
+                </li>
+              </ul>
+            </nav>
           </div>
         </div>
       </div>
@@ -63,5 +95,11 @@
 
 </script>
 <style scoped>
-  
+  .active a {
+    color: white;
+  }
+
+  .alarm-body {
+    height: 755px;
+  }
 </style>
