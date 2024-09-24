a69f4ca3d6f0a7f1e66ff89d6c9434e8cadd841a
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-09-24 20:27:57 +0900

    Backend: 알림 페이지 기본 조회 갯수 직접 지정

--- src/main/java/com/doubles/selfstudy/controller/AlarmController.java
+++ src/main/java/com/doubles/selfstudy/controller/AlarmController.java
@@ -6,6 +6,7 @@ import com.doubles.selfstudy.service.AlarmService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,10 @@ public class AlarmController {
     private final AlarmService alarmService;
 
     @GetMapping
-    public Response<Page<AlarmResponse>> alarmList(Authentication authentication, Pageable pageable) {
+    public Response<Page<AlarmResponse>> alarmList(
+            Authentication authentication,
+            @PageableDefault(size = 10) Pageable pageable
+    ) {
         return Response.success(
                 alarmService.alarmList(authentication.getName(), pageable)
                         .map(AlarmResponse::fromAlarmDto)
