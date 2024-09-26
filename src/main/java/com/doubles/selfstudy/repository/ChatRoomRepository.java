17266b64f1d16e73a2b8aa5b3a289edc9eb24fe7
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-09-26 19:11:18 +0900

    Backend: 닉네임으로 유저 조회가 안되는 부분 수정

--- src/main/java/com/doubles/selfstudy/repository/ChatRoomRepository.java
+++ src/main/java/com/doubles/selfstudy/repository/ChatRoomRepository.java
@@ -32,12 +32,14 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
             "       (SELECT MAX(cm2.createdAt) FROM ChatMessage cm2 WHERE cm2.chatRoom = cr) " +
             "WHERE (u1 = :user OR u2 = :user) " +
             "AND (CASE WHEN u1 = :user THEN u2.nickname ELSE u1.nickname END) LIKE %:nickname% " +
-            "AND (:userId IS NULL OR cr.leaveUserId != :userId)")
+            "AND (cr.leaveUserId IS NULL OR cr.leaveUserId != :userId)")
     List<Object[]> findAllChatRoomsByUserAndNickname(@Param("user") UserAccount user, @Param("userId") String userId, @Param("nickname") String nickname);
-
+    
+    // 유저 엔티티로 채팅룸 검색
     @Query("SELECT cr FROM ChatRoom cr WHERE (cr.user1 = :user1 AND cr.user2 = :user2) OR (cr.user1 = :user2 AND cr.user2 = :user1)")
     Optional<ChatRoom> findByUsers(@Param("user1") UserAccount user1, @Param("user2") UserAccount user2);
 
+    // 유저id로 채팅룸 검색
     @Query("SELECT cr FROM ChatRoom cr WHERE (cr.user1.userId = :userId1 AND cr.user2.userId = :userId2) OR (cr.user1.userId = :userId2 AND cr.user2.userId = :userId1)")
     Optional<ChatRoom> findByUserIds(@Param("userId1") String userId1, @Param("userId2") String userId2);
 }
