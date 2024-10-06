b320bb26e52f456ecdd39ebefa696548ba809435
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-10-06 17:02:09 +0900

    Backend: ErrorPageController 수정

--- src/main/java/com/doubles/selfstudy/controller/ErrorPageController.java
+++ src/main/java/com/doubles/selfstudy/controller/ErrorPageController.java
@@ -3,12 +3,18 @@ package com.doubles.selfstudy.controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ErrorPageController implements ErrorController {
 
-    @GetMapping("/error")
-    public String redirectRoot() {
-        return "index.html";
+    //    @GetMapping("/error")
+    //    public String redirectRoot() {
+    //        return "index.html";
+    //    }
+    @RequestMapping("/error") // Spring Boot의 기본 에러 핸들러
+    public String handleError() {
+        // 에러가 발생하면 index.html로 포워딩
+        return "forward:/index.html";
     }
 }
