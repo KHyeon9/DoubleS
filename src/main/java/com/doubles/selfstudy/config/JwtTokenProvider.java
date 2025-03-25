e3e3d6e32197b79356aa83f4e977c6df6da7242f
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-03-25 17:32:28 +0900

    [JWT] JwtTokenUtils을 사용하고 provider를 사용 안하고 있으므로 provider를 사용하기 위한 수정

--- src/main/java/com/doubles/selfstudy/config/JwtTokenProvider.java
+++ src/main/java/com/doubles/selfstudy/config/JwtTokenProvider.java
@@ -1,15 +1,13 @@
 package com.doubles.selfstudy.config;
 
 import com.doubles.selfstudy.dto.user.UserAccountDto;
-import com.doubles.selfstudy.service.UserAccountService;
 import com.doubles.selfstudy.utils.JwtTokenUtils;
+import com.doubles.selfstudy.utils.ServiceUtils;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -22,7 +20,7 @@ public class JwtTokenProvider {
     @Value("${jwt.token.expired-time-ms}")
     private Long expiredTimeMs;
 
-    private final UserAccountService userAccountService;
+    private final ServiceUtils serviceUtils;
 
     // 토큰 생성
     public String createToken(String userId) {
@@ -31,7 +29,7 @@ public class JwtTokenProvider {
 
     // 토큰 검증
     public boolean validateToken(String token) {
-        return !JwtTokenUtils.isExpired(token, secretKey);
+        return JwtTokenUtils.isExpired(token, secretKey);
     }
 
     // 토큰에서 Authentication 객체 생성
@@ -39,14 +37,8 @@ public class JwtTokenProvider {
         Claims claims = JwtTokenUtils.extractClaims(token, secretKey);
         String userId = (String) claims.get("userId");
 
-        UserAccountDto userAccountDto = UserAccountDto.fromEntity(userAccountService.loadUserByUserId(userId));
+        UserAccountDto userAccountDto = UserAccountDto.fromEntity(serviceUtils.getUserAccountOrException(userId));
 
-        UserDetails userDetails = new User(
-                userAccountDto.getUserId(),
-                userAccountDto.getPassword(),
-                userAccountDto.getAuthorities()
-        );
-
-        return new UsernamePasswordAuthenticationToken(userDetails, token, userDetails.getAuthorities());
+        return new UsernamePasswordAuthenticationToken(userAccountDto, null, userAccountDto.getAuthorities());
     }
 }
