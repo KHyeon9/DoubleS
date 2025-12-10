0276a21969a91300392e903bb9890e701dcd1b04
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-12-11 00:59:20 +0900

    Backend: principal이 Message Send시에 null로 찍히는 부분을 해결하기 위해 수정 및 의존성 주입으로 사용하기 위한 Component 어노테이션 추가
    StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);을
    StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message,StompHeaderAccessor.class);
    로 변경

--- src/main/java/com/doubles/selfstudy/config/JwtChannelInterceptor.java
+++ src/main/java/com/doubles/selfstudy/config/JwtChannelInterceptor.java
@@ -9,10 +9,13 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 @RequiredArgsConstructor
 public class JwtChannelInterceptor implements ChannelInterceptor  {
 
@@ -23,7 +26,9 @@ public class JwtChannelInterceptor implements ChannelInterceptor  {
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         // 헤더 가져옴
-        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        // StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        StompHeaderAccessor accessor = MessageHeaderAccessor
+                .getAccessor(message, StompHeaderAccessor.class);
         // 코멘트 가져오기
         StompCommand command = accessor.getCommand();
 
